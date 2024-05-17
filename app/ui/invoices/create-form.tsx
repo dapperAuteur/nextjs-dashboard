b129900db851dd50359f9740e19874b45e2ddf32
dapperAuteur <a@awews.com>
2024-05-16 20:58:03 -0700

    complete ch 12 and prepare for ch13 Handling Errors

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -10,9 +10,8 @@ import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
-  <form action={createInvoice}></form>
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
